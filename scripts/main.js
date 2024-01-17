c5bfb4d258a04e77609c752bcb3fddc801710aa3
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2024-01-17 10:04:39 +0100

    Finalizamos la función de validación del formulario

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,57 @@
  * JAIME FERNÁNDEZ CALVO
  */
 
-const nombre = document.getElementById("nombre");
-const correo = document.getElementById("correo");
-const adInfo = document.getElementById("adInfo");
-const reserva = document.getElementById("reserva");
-const info = document.getElementById("info");
-const txtmsj = document.getElementById("txtmsj");
-const enviar = document.getElementById("enviar");
-const errores = document.getElementById("errores");
-errores = [];
+const formulario = document.querySelector("#formulario");
+const nombre = document.querySelector("#nombre");
+const correo = document.querySelector("#correo");
+const adInfo = document.querySelector("#adInfo");
+const reserva = document.querySelector("#reserva");
+const info = document.querySelector("#info");
+const txtmsj = document.querySelector("#txtmsj");
+const errores = document.querySelector("#errores");
+let msjerrores = [];
 
+
+
+const validar = evento =>{
+    evento.preventDefault();
+    msjerrores = [];
+
+    if(nombre.value.trim().length === 0){
+        msjerrores.push('El nombre es un campo obligatorio');
+    } 
+
+    if(!/^[a-zA-Z0-9]*$/.test(nombre.value.trim())){
+        msjerrores.push('Un nombre propio comienza siempre por una letra mayúscula y no contiene números');
+    }
+
+
+    if(!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(correo.value.trim())){
+        msjerrores.push('Introduzca una dirección de correo válida');
+    }
+
+    if(txtmsj.value.trim() < 10){
+        msjerrores.push('Mensaje demasiado corto');
+    }
+
+    if (msjerrores.length === 0 && confirm("¿Desea enviar el formulario?")){
+        formulario.submit();
+    } else if (msjerrores.length > 0){
+        errores.textContent = "";
+        console.log(msjerrores);
+        msjerrores.forEach(function(mensaje){
+            const miLi = document.createElement("li");
+            miLi.textContent = mensaje;
+            errores.appendChild(miLi);
+        })
+    }
+}
+
+
+const informacion = evento =>{
+
+}
+
+//eventos
+info.addEventListener("submit", informacion);
+formulario.addEventListener("submit", validar);
