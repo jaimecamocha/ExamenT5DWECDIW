c3ab164db7777dedbb39dd5e7e0e98f98192bdc6
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2024-01-17 09:22:13 +0100

    Empezamos a codificar el archivo JavaScript

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+/**
+ * JAIME FERNÁNDEZ CALVO
+ */
+
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("correo");
+const adInfo = document.getElementById("adInfo");
+const reserva = document.getElementById("reserva");
+const info = document.getElementById("info");
+const txtmsj = document.getElementById("txtmsj");
+const enviar = document.getElementById("enviar");
+const errores = document.getElementById("errores");
+errores = [];
+
